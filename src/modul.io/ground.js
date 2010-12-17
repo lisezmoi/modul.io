bd103a0aa28de5804fe46646b239c6273d47f5f7
Pierre Bertet <bonjour@pierrebertet.net>
2010-12-17 02:44:48 +0100

    Removed rainbow ground

--- src/modul.io/ground.js
+++ src/modul.io/ground.js
@@ -17,34 +17,6 @@ var Script = process.binding('evals').Script,
                 ctx.fillRect(x+24.5, y+24.5, 1, 1);
                 callback();
             },
-            rainbow: function(ctx, x, y, callback) {
-                // The hue spectrum used by HSV color picker charts.
-                var color, hue = [
-                  [255,   0,   0 ], // 0, Red,       0°
-                  [255, 255,   0 ], // 1, Yellow,   60°
-                  [  0, 255,   0 ], // 2, Green,   120°
-                  [  0, 255, 255 ], // 3, Cyan,    180°
-                  [  0,   0, 255 ], // 4, Blue,    240°
-                  [255,   0, 255 ], // 5, Magenta, 300°
-                  [255,   0,   0]], // 6, Red,     360°
-                  
-                  // Create the linear gradient: sx, sy, dx, dy.
-                  // That's the start (x,y) coordinates, followed by the destination (x,y).
-                  gradient = ctx.createLinearGradient(x, y, x+50, y);
-                  
-                // Add the color stops.
-                for (var i = 0; i <= 6; i++) {
-                  color = 'rgb(' + hue[i][0] + ', ' + hue[i][1] + ', ' + hue[i][2] + ')';
-                  gradient.addColorStop(i * 1/6, color);
-                }
-                
-                // Use the gradient for the fillStyle.
-                ctx.fillStyle = gradient;
-                
-                ctx.fillRect(x+19.5, y, 10, 50);
-                ctx.fillRect(x, y+19.5, 50, 10);
-                callback();
-            },
             blank: function(ctx, x, y, callback) {
                 ctx.fillStyle="#fff";
                 callback();
