a4d1922e0483ac0aa61a593b3cdd47500884299b
Pierre Bertet <bonjour@pierrebertet.net>
2011-06-12 17:18:00 +0200

    Ace editor: script src

---
+++ src/static/scripts/mio.console.js
@@ -0,0 +1,41 @@
+(function(){
+    var mio = window.mio = window.mio || {};
+    
+    mio.console = (function(){
+        var pub = {},
+            consolePanel,
+            consoleElt,
+            lines = [];
+        
+        pub.init = function(panel, msg) {
+            consolePanel = panel;
+            
+            // Clear button
+            var buttonContainer = mio.util.createElt("div");
+            var button = mio.util.createElt("button", {textContent: "Clear", events: {
+                click: pub.clear
+            }});
+            buttonContainer.appendChild(button);
+            panel.contentElt.appendChild(buttonContainer);
+            
+            // Console element
+            consoleElt = mio.util.createElt("div");
+            consoleElt.className = "console";
+            panel.contentElt.appendChild(consoleElt);
+            
+            pub.log(msg);
+        };
+        
+        pub.log = function(msg) {
+            var line = mio.util.createElt("pre");
+            line.textContent = msg;
+            consoleElt.appendChild(line);
+        };
+        
+        pub.clear = function() {
+            consoleElt.innerHTML = "";
+        };
+        
+        return pub;
+    })();
+})();
