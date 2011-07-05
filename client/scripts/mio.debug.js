2bf0f9926e95215b2e629b99e0a0b467ae509140
Pierre Bertet <bonjour@pierrebertet.net>
2011-07-05 21:20:45 +0200

    Tabs -> spaces

--- client/scripts/mio.debug.js
+++ client/scripts/mio.debug.js
@@ -7,16 +7,17 @@
         var showScreen = function(marginX, marginY) {
             document.getElementById("world").style.outline = "1px solid #fff";
             pub.debugScreen = document.getElementById("world-container");
-    		pub.debugScreen.style.position = "absolute";
-    		pub.debugScreen.style.top = (marginY) + "px";
-    		pub.debugScreen.style.left = (marginX) + "px";
-    		pub.debugScreen.style.width = (window.innerWidth-marginX*2) + "px";
-    		pub.debugScreen.style.height = (window.innerHeight-marginY*2) + "px";
-    		pub.debugScreen.style.border = "1px solid white";
-    		
-    		window.addEventListener("resize", function(){
+            pub.debugScreen.style.position = "absolute";
+            pub.debugScreen.style.top = (marginY) + "px";
+            pub.debugScreen.style.left = (marginX) + "px";
+            pub.debugScreen.style.width = (window.innerWidth-marginX*2) + "px";
+            pub.debugScreen.style.height = (window.innerHeight-marginY*2) + "px";
+            pub.debugScreen.style.border = "1px solid white";
+            pub.debugScreen.style.overflow = "visible";
+            
+            window.addEventListener("resize", function(){
                 pub.debugScreen.style.width = (window.innerWidth-marginX*2)+"px";
-        		pub.debugScreen.style.height = (window.innerHeight-marginY*2)+"px";
+                pub.debugScreen.style.height = (window.innerHeight-marginY*2)+"px";
             }, false);
         };
         
