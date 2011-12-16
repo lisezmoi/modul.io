9505e25c9a4b8ce45ee83b61e24c2b9345d3eecb
Pierre Bertet <bonjour@pierrebertet.net>
2011-12-16 23:36:40 +0100

    Removed unused require("sys")

--- scripts/start.js
+++ scripts/start.js
@@ -3,7 +3,6 @@
 // Uncomment this line to launch modul.io in dev mode
 // process.env.NODE_ENV = 'dev';
 
-var sys = require('sys');
 var modulio = require('../server/modul.io.js');
 
 modulio.start();
