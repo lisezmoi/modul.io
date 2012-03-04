904128ba4b2957b5ecccc0495b66e779885e069d
Pierre Bertet <bonjour@pierrebertet.net>
2012-03-04 17:53:45 +0100

    Js minification when the server starts

--- server/lib/web.js
+++ server/lib/web.js
@@ -4,14 +4,52 @@ var express = require('express'),
     util = require('util'),
     server = null;
 
+function compressJsFiles(callback) {
+  var jsp = require("uglify-js").parser,
+      pro = require("uglify-js").uglify,
+      scripts = require(__dirname + '/../../client/scripts.json'),
+      fs = require('fs'),
+      originalCode = '',
+      ast = null,
+      filesCount = scripts.app.length,
+      filesRead = 0;
+
+  function writeAppFile(data) {
+    fs.writeFile(__dirname + '/../../client/scripts/mio.js', data, 'utf8', function(err) {
+      if (err) throw err;
+      if (callback) callback();
+    });
+  }
+
+  function compress(data) {
+    ast = jsp.parse(data);
+    ast = pro.ast_mangle(ast);
+    ast = pro.ast_squeeze(ast);
+    writeAppFile(pro.gen_code(ast));
+  }
+
+  for (var i=0; i < filesCount; i++) {
+      fs.readFile(__dirname + '/../../client/scripts/' + scripts.app[i], 'utf8', function(err, data) {
+          if (err) throw err;
+          originalCode += data;
+          filesRead++;
+          if (filesRead === filesCount) {
+            compress(originalCode);
+          }
+      });
+  }
+}
+
 exports.start = function(port, hostname) {
-    
+
     if (server) {
         return server;
     }
-    
+
+    compressJsFiles();
+
     server = express.createServer();
-    
+
     server.configure(function(){
         server.use(express.methodOverride());
         server.use(express.bodyParser());
@@ -21,26 +59,26 @@ exports.start = function(port, hostname) {
         server.use(express.favicon(__dirname + '/../../client/favicon.ico'));
         server.use(express.session({ 'secret': 'goVfq36*jp586w%GMkW)7F#,x2>y_gPu)tUAZ`%$I:#4S.a!z-x[W6M?gn/I.}ks' }));
     });
-    
+
     // Dev environment
     server.configure('dev', function(){
         server.set('views', __dirname + '/../views');
         server.set('view engine', 'ejs');
         server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
     });
-    
+
     // Prod environment
     server.configure('prod', function(){
         server.set('views', __dirname + '/../views');
         server.set('view engine', 'ejs');
         server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
     });
-    
+
     // Views global vars
     var viewVars = {
-        
+
     };
-    
+
     // Home
     server.get('/', function(req, res, next) {
         res.render('index.ejs', {
@@ -49,7 +87,7 @@ exports.start = function(port, hostname) {
             moduls: world.getModuls()
         });
     });
-    
+
     // Get ground sprites
     server.get('/get/ground', function(req, res, next) {
         world.ground.getGroundsPng(function(buf) {
@@ -57,7 +95,7 @@ exports.start = function(port, hostname) {
             res.end(buf);
         });
     });
-    
+
     // Get a modul image
     server.get('/:user/:modul/skin', function(req, res, next) {
         var modul = world.getModul(req.params.user + "/" + req.params.modul);
@@ -70,10 +108,10 @@ exports.start = function(port, hostname) {
             next();
         }
     });
-    
+
     // Modul page
     server.get('/:user/:modul', function(req, res, next) {
-        
+
         if ( !world.getModul(req.params.user + '/' + req.params.modul) ) { // Modul exists?
             res.render('create-modul.ejs', {
                 layout: false,
@@ -95,7 +133,7 @@ exports.start = function(port, hostname) {
             });
         }
     });
-    
+
     // Modul creation
     server.post('/:user/:modul', function(req, res, next) {
         var modulId = req.params.user + '/' + req.params.modul;
@@ -108,7 +146,7 @@ exports.start = function(port, hostname) {
             res.redirect('/' + modulId);
         }
     });
-    
+
     // NotFound Exception
     function NotFound(msg){
         this.name = 'NotFound';
@@ -116,12 +154,12 @@ exports.start = function(port, hostname) {
         Error.captureStackTrace(this, arguments.callee);
     }
     util.inherits(NotFound, Error);
-    
+
     // 404
     server.get('/*', function(req, res, next){
         throw new NotFound();
     });
-    
+
     // Errors
     server.error(function(err, req, res, next) {
         if (err instanceof NotFound) {
@@ -135,9 +173,9 @@ exports.start = function(port, hostname) {
             });
         }
     });
-    
+
     server.listen(port, hostname);
     logger.info('webserver started at http://127.0.0.1:'+port);
-    
+
     return server;
 };
